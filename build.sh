931fe0098c63d40201126b132365cb19882b4ab0
vagabund3219 <vagabund3219@gmail.com>
2022-12-19 20:54:58 +0300

    otkat

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@
 python3.9 -m -pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate --noinput
-python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
