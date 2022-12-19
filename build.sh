e8c1ca597189a7cf5facbe2a919a70aaa75c53c3
vagabund3219 <vagabund3219@gmail.com>
2022-12-19 21:05:41 +0300

    rfw

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 
-<<<<<<< HEAD
+
 echo "build start"
-python3.9 -m pip install -r requirements.txt
-=======
+
 python3.9 -m -pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate --noinput
->>>>>>> oktak
 python3.9 manage.py collectstatic --noinput --clear
 echo "build end"
 
