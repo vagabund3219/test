09d0d4189a1283f676804b07874edc6abef0a613
vagabund3219 <vagabund3219@gmail.com>
2022-12-19 21:47:31 +0300

    попытка postgres

--- kursovoiProekt/settings.py
+++ kursovoiProekt/settings.py
@@ -80,10 +80,20 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'iJGddO7tcJFD2EMcde6t',
+        'HOST': 'containers-us-west-165.railway.app',
+        'PORT': '6945',
     }
 }
 # DATABASES['default'] = dj_database_url.config()
